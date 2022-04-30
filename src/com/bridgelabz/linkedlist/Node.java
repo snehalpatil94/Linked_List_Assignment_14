24e1c77b0e532de026ba99f1ff1b76f00df641b4
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-30 16:48:53 +0530

    Node Class file

---
+++ src/com/bridgelabz/linkedlist/Node.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.linkedlist;
+
+public class Node {
+	// variable declaration
+	public int key;
+	public Node next;
+
+	// constructor
+	public Node(int key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	// getter and setter
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
