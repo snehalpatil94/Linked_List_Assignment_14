52872b3a660d0fb528bb56cc64125cd59d524af9
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-30 16:50:36 +0530

    Added add method

---
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList {
+	// class variable declaration
+	Node head;
+	Node tail;
+
+	// Constructor
+	public MyLinkedList() {
+		super();
+		this.head = null;
+		this.tail = null;
+	}
+
+	// Method to add node in linked list
+	public void add(int key) {
+		Node newNode = new Node(key);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			Node temp = head;
+			this.head = newNode;
+			newNode.next = temp;
+		}
+	}
+
+	// Method to print node of linked list
+	public void printNode() {
+		Node temp = head;
+		while (temp.next != null) {
+			System.out.print(temp.key + "->");
+			temp = temp.next;
+		}
+		System.out.println(temp.key);
+		System.out.println("--------------------------------------------------");
+	}
+}
